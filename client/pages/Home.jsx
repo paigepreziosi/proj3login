9b962c78308281c8b9e05847d1158ea685a17501
paigepreziosi <pvpreziosi@hotmail.com>
2024-03-09 11:31:29 -0500

    commit home

---
+++ client/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            <img
+                src="./src/pages/AMRAP2.png" 
+                alt="Welcome to AMRAP"
+                style={{ width: '80%', height: 'auto' }}
+            />
+        </div>
+    );
+}
